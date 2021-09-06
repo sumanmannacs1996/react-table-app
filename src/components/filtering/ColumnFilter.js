a771cbe5c7360ddd21d2246a143a6c89453a9bc6
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-09-07 01:41:48 +0530

    Added code of Debounce for Column Filter

--- src/components/filtering/ColumnFilter.js
+++ src/components/filtering/ColumnFilter.js
@@ -2,10 +2,21 @@ import React from 'react'
 
 function ColumnFilter({column}) {
     const {filterValue,setFilter} = column;
+
+    const debounceFunction =(fn,delay)=>{
+        let timer;
+        return (e)=>{
+            clearTimeout(timer);
+            timer = setTimeout(()=>{
+                fn(e.target.value);
+            },delay)
+        }
+    }
+
     return (
         <span>
           Search:{''}
-          <input onChange={(e)=>setFilter(e.target.value)}/>  
+          <input onChange={debounceFunction(setFilter,400)}/>  
         </span>
     )
 }
